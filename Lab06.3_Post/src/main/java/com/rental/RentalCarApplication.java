7f2684157e37c2fd48ad079911fa6eaad1905edc
boozycat <domderek60@yahoo.com>
2020-01-20 14:19:54 -0500

    Added a little comment

--- Lab06.3_Post/src/main/java/com/rental/RentalCarApplication.java
+++ Lab06.3_Post/src/main/java/com/rental/RentalCarApplication.java
@@ -17,7 +17,7 @@ public class RentalCarApplication {
 		SpringApplication app = new SpringApplication(RentalCarApplication.class);
 		app.addInitializers(new ProfileInitializer());
 		app.run(args);
-        //add a comment here as it is important
+        //add a comment here as it is important and some more comments
 		String BASE_URL = "http://localhost:8081/";
 		RentalCar car = new RentalCar("ORD", "SUV", 99.99, "Ford", "Explorer");
 
