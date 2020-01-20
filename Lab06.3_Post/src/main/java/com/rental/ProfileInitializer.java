f7581f50754f1c7c8513d810724113bc91e25489
boozycat <domderek60@yahoo.com>
2020-01-20 08:16:22 -0500

    added a comment

--- Lab06.3_Post/src/main/java/com/rental/ProfileInitializer.java
+++ Lab06.3_Post/src/main/java/com/rental/ProfileInitializer.java
@@ -3,7 +3,7 @@ package com.rental;
 import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.core.env.ConfigurableEnvironment;
-
+//adding a comment here
 public class ProfileInitializer
 		implements ApplicationContextInitializer<AnnotationConfigServletWebServerApplicationContext> {
 	public void initialize(AnnotationConfigServletWebServerApplicationContext context) {
